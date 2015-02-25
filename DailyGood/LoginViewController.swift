05c61fef69eb3b6e6fdda47fcbe7900c0653fd8a
mowoods <maurice@inneractproject.org>
2015-02-25 13:02:06 -0800

    quick change

--- DailyGood/LoginViewController.swift
+++ DailyGood/LoginViewController.swift
@@ -32,34 +32,22 @@ class LoginViewController: UIViewController {
 
     @IBAction func loginButton(sender: AnyObject) {
         
-        
-        
-        if (usernameTextField.text == "mo") {
-        
-        } else {
-            LoginButton.alpha = 0
-            disableLoginButton.alpha = 1
-            var alertView = UIAlertView(title: "Incorrect Username", message: "Please try again", delegate: nil, cancelButtonTitle: "OK")
-            alertView.show()
-            usernameTextField.text = ""
-            
-        }
-        
         if (usernameTextField.text.isEmpty) {
             var alertView = UIAlertView(title: "Opps", message: "Please enter a username", delegate: nil, cancelButtonTitle: "OK")
             alertView.show()
         }
-        
+
         loaderIndicator.startAnimating()
         delay(1, { () -> () in
-            if (self.passwordTextField.text == "dailygood") {
+            if (self.passwordTextField.text == "dailygood" && self.usernameTextField.text == "mo" || self.usernameTextField.text == "Kelly" || self.usernameTextField.text == "Fillip") {
                 self.performSegueWithIdentifier("LoginSegue", sender: nil)
                 
             } else {
-                var alertView = UIAlertView(title: "Incorrect Password", message: "Please try entering your password again", delegate: nil, cancelButtonTitle: "OK")
+                var alertView = UIAlertView(title: "Incorrect Username or Password", message: "Please try entering your information again", delegate: nil, cancelButtonTitle: "OK")
                 alertView.show()
                 self.loaderIndicator.stopAnimating()
                 self.passwordTextField.text = ""
+                self.usernameTextField.text = ""
             }
         })
 
